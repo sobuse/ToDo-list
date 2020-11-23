08f12b379619ca4d5552c0514e0f4db8e422d9fd
Ehueriken Samuel Obuse <ehuerikenbaba@gmail.com>
2020-11-23 21:25:32 +0000

    initail to do list

---
+++ date.js
@@ -0,0 +1,18 @@
+exports.getDate = function () {
+  const today = new Date();
+  const option = {
+    weekdays: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  return today.toLocaleDateString("en-US", option);
+};
+
+exports.getDay = function () {
+  const today = new Date();
+  const option = {
+    weekday: "long",
+  };
+  return today.toLocaleDateString("en-US", options);
+};
